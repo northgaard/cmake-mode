7e56c0ed8fb5d36bd4d3f93134c5adc299671855
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2022-08-10 11:02:31 +0200

    Be a bit better about the license

--- cmake-mode.el
+++ cmake-mode.el
@@ -2,8 +2,8 @@
 
 ;; Package-Requires: ((emacs "24.1"))
 
-; Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
-; file Copyright.txt or https://cmake.org/licensing for details.
+; Distributed under the OSI-approved BSD 3-Clause License. See
+; accompanying LICENSE file.
 
 ;------------------------------------------------------------------------------
 
